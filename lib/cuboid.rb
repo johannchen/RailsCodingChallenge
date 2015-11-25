fd18ac128ac175c1f82e82ae92ac85575fc25965
Johann Chen <johannchen@gmail.com>
2015-11-25 01:26:00 -0800

    intersects with another cuboid

--- lib/cuboid.rb
+++ lib/cuboid.rb
@@ -1,17 +1,51 @@
 
 class Cuboid
-  
+  attr_accessor :x,:y,:z,:length,:width,:height
+
+  def initialize(x=0, y=0, z=0, l=1, w=1, h=1)
+    @x,@y,@z = x,y,z
+    @length,@width,@height = l,w,h
+  end
   #BEGIN public methods that should be your starting point
 
   def move_to!(x, y, z)
-  end
-  
-  def vertices
-  end
-  
-  #returns true if the two cuboids intersect each other.  False otherwise.
-  def intersects?(other)
+    @x,@y,@z=x,y,z
+    true
   end
 
-  #END public methods that should be your starting point  
+  def vertices
+    corners = []
+    x,y,z = @x,@y,@z
+    l,w,h = @length,@width,@height
+    corners << [x, y, z]
+    corners << [x, y, z+h]
+    corners << [x, y+w, z]
+    corners << [x, y+w, z+h]
+    corners << [x+l, y, z]
+    corners << [x+l, y, z+h]
+    corners << [x+l, y+w, z]
+    corners << [x+l, y+w, z+h]
+    corners
+  end
+
+  #returns true if the two cuboids intersect each other.  False otherwise.
+  def intersects?(other)
+    overlap?(@x, @length, other.x, other.length) &&
+    overlap?(@y, @width, other.y, other.width) &&
+    overlap?(@z, @height, other.z, other.height)
+  end
+
+  #END public methods that should be your starting point
+  private
+  # check intersect in one dimension or a straight line
+  # x1 and x2 are the starting points
+  def overlap?(x1, length1, x2, length2)
+    if x1 < x2
+      x1 + length1 > x2 ? true : false
+    elsif x1 > x2
+      x2 + length2 > x1 ? true : false
+    else
+      true
+    end
+  end
 end
--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -1,17 +1,54 @@
 require 'cuboid'
 
-#This test is incomplete and, in fact, won't even run without errors.  
+#This test is incomplete and, in fact, won't even run without errors.
 #  Do whatever you need to do to make it work and please add your own test cases for as many
 #  methods as you feel need coverage
 describe Cuboid do
- 
-  describe "move_to" do
-    it "changes the origin in the simple happy case" do
-      expect(subject.move_to!(1,2,3)).to be true
+  describe "#initialize" do
+    it "creats a cuboid" do
+      cube = Cuboid.new(3,2,1,4,5,6)
+      expect(cube.x).to eq(3)
+      expect(cube.y).to eq(2)
+      expect(cube.z).to eq(1)
+      expect(cube.length).to eq(4)
+      expect(cube.width).to eq(5)
+      expect(cube.height).to eq(6)
     end
-  end    
-  
-  describe "intersects?" do
   end
 
+  describe "move_to" do
+    it "changes the origin in the simple happy case" do
+      #expect(subject.move_to!(1,2,3)).to be true
+      subject.move_to!(1,2,3)
+      expect(subject.x).to eq(1)
+      expect(subject.y).to eq(2)
+      expect(subject.z).to eq(3)
+    end
+  end
+
+  describe "#vertices" do
+    it "stores the vertices in an array" do
+      expect(subject.vertices).to eq([[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]])
+    end
+  end
+
+  describe "intersects?" do
+    it "does not intersect with another cuboid" do
+      c = Cuboid.new(2,2,2,2,3,4)
+      expect(subject.intersects?(c)).to be false
+    end
+    it "does not intersect when sharing a side with another cuboid" do
+      c = Cuboid.new(1,0,0,1,1,1)
+      expect(subject.intersects?(c)).to be false
+    end
+    it "intersects inside another cuboid" do
+      c = Cuboid.new(0,0,0,2,3,4)
+      expect(subject.intersects?(c)).to be true
+    end
+    it "intersects partially with another cuboid" do
+      c1 = Cuboid.new(0,0,0,2,3,4)
+      c2 = Cuboid.new(1,1,1,2,3,4)
+      expect(c1.intersects?(c2)).to be true
+    end
+  end
 end
